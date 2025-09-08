2c442cc3d7e8dbef0e8071f5a8c752ad90bc84ee
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-09-08 12:20:59 +0200

    Added version support configuration for the underlying client #1

--- http-client/src/main/java/com/jetbrains/test/boot4/client/QuoteClient.java
+++ http-client/src/main/java/com/jetbrains/test/boot4/client/QuoteClient.java
@@ -1,6 +1,10 @@
 package com.jetbrains.test.boot4.client;
 
 import com.jetbrains.test.boot4.http.sdk.Quote;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.ApiVersionInserter;
+import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
@@ -10,4 +14,15 @@ interface QuoteClient {
     @GetExchange(url = "/quote")
     Quote fetchRandomQuote();
 
+}
+
+@Configuration(proxyBeanMethods = false)
+class HttpClientsConfiguration {
+
+    @Bean
+    public RestClientHttpServiceGroupConfigurer groupConfigurer() {
+        return groups -> groups.forEachClient((group, builder) ->
+                builder.apiVersionInserter(ApiVersionInserter.useHeader("X-API-Version"))
+        );
+    }
 }
