76587166da7b7cba0d5624ab25f5166469debbde
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-09-22 20:18:02 +0200

    Moved all classes to the same class for demo purposes

--- http-client/src/main/java/com/jetbrains/test/boot4/client/HttpClientApplication.java
+++ http-client/src/main/java/com/jetbrains/test/boot4/client/HttpClientApplication.java
@@ -1,19 +1,45 @@
 package com.jetbrains.test.boot4.client;
 
 import com.jetbrains.test.boot4.http.sdk.Quote;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.service.registry.HttpServiceClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @SpringBootApplication
 @ImportHttpServices(QuoteClient.class)
 class HttpClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HttpClientApplication.class, args);
+    }
+}
+
+@HttpExchange(url = "http://localhost:8080/api", accept = "application/json")
+interface QuoteClient {
+
+    @GetExchange(url = "/quote")
+    Quote fetchRandomQuote();
+
+}
+
+@RestController
+@RequestMapping("/api")
+class QuoteController {
+
+    private final QuoteClient client;
+
+    QuoteController(QuoteClient client) {
+        this.client = client;
+    }
+
+    @GetMapping("quote")
+    public ResponseEntity<Quote> getQuote() {
+        return ResponseEntity.ok(client.fetchRandomQuote());
     }
 
 }
