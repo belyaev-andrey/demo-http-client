730ff954c686e4255016f92fc11ada2b62999eeb
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-09-22 20:35:40 +0200

    Added multi-versioning to Java Client

--- http-client/src/main/java/com/jetbrains/test/boot4/client/HttpClientApplication.java
+++ http-client/src/main/java/com/jetbrains/test/boot4/client/HttpClientApplication.java
@@ -3,19 +3,30 @@ package com.jetbrains.test.boot4.client;
 import com.jetbrains.test.boot4.http.sdk.Quote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ApiVersionInserter;
+import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.registry.ImportHttpServices;
 
+import java.util.List;
+
 @SpringBootApplication
-@ImportHttpServices(QuoteClient.class)
 class HttpClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(HttpClientApplication.class, args);
+    }
+
+    @Bean
+    public RestClientHttpServiceGroupConfigurer groupConfigurer() {
+        return groups -> groups.forEachClient((group, builder) ->
+                builder.apiVersionInserter(ApiVersionInserter.useHeader("X-API-Version"))
+        );
     }
 }
 
@@ -24,6 +35,9 @@ interface QuoteClient {
 
     @GetExchange(url = "/quote")
     Quote fetchRandomQuote();
+
+    @GetExchange(url = "/quote", version = "2.0")
+    List<Quote> fetchRandomQuotes();
 
 }
 
@@ -36,10 +50,13 @@ class QuoteController {
     QuoteController(QuoteClient client) {
         this.client = client;
     }
-
     @GetMapping("quote")
     public ResponseEntity<Quote> getQuote() {
         return ResponseEntity.ok(client.fetchRandomQuote());
+    }
+    @GetMapping("quotes")
+    public ResponseEntity<List<Quote>> getQuotes() {
+        return ResponseEntity.ok(client.fetchRandomQuotes());
     }
 
 }
