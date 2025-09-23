94b37db823da32d729b4d24a9a19c47ea6c2f612
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-09-23 20:21:24 +0200

    Removed unused imports

--- http-klient/src/main/kotlin/com/jetbrains/test/boot4/klient/HttpKlientApplication.kt
+++ http-klient/src/main/kotlin/com/jetbrains/test/boot4/klient/HttpKlientApplication.kt
@@ -3,13 +3,9 @@ package com.jetbrains.test.boot4.klient
 import com.jetbrains.test.boot4.http.sdk.Quote
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.ApiVersionInserter
-import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.registry.HttpServiceGroup
