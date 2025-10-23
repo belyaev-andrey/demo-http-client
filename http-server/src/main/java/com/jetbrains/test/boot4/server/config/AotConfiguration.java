85dfe6a2b3d430839fe126f48ef68c1a7d98dcf2
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-10-23 17:04:12 +0200

    Improved AOT configuration leveraging the latest framework changes

--- http-server/src/main/java/com/jetbrains/test/boot4/server/config/AotConfiguration.java
+++ http-server/src/main/java/com/jetbrains/test/boot4/server/config/AotConfiguration.java
@@ -2,22 +2,11 @@ package com.jetbrains.test.boot4.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jdbc.core.convert.JdbcConverter;
-import org.springframework.data.jdbc.core.convert.MappingJdbcConverter;
 import org.springframework.data.jdbc.core.dialect.JdbcDialect;
 import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
-import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 
-@Profile("aot")
 @Configuration
 public class AotConfiguration {
-
-    @Bean
-    JdbcConverter jdbcConverter(RelationalMappingContext context) {
-        return new MappingJdbcConverter(context, (identifier, path) -> null);
-    }
-
     @Bean
     JdbcDialect dialect() {
         return JdbcPostgresDialect.INSTANCE;
