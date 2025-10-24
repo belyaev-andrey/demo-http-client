3990c26aa1fb8915fa3a0c27815de23f3ed26a0e
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-10-24 13:46:29 +0200

    Fixed client versioning

--- http-client/src/main/java/com/jetbrains/test/boot4/client/HttpClientApplication.java
+++ http-client/src/main/java/com/jetbrains/test/boot4/client/HttpClientApplication.java
@@ -3,10 +3,14 @@ package com.jetbrains.test.boot4.client;
 import com.jetbrains.test.boot4.http.sdk.Quote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.restclient.RestClientCustomizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.registry.ImportHttpServices;
@@ -24,7 +28,7 @@ class HttpClientApplication {
 @HttpExchange(url = "http://localhost:8080/api", accept = "application/json")
 interface QuoteClient {
 
-    @GetExchange(url = "/quote")
+    @GetExchange(url = "/quote", version = "1.0")
     Quote fetchRandomQuote();
 
     @GetExchange(url = "/quote", version = "2.0")
@@ -33,7 +37,7 @@ interface QuoteClient {
 }
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", consumes = "application/json", produces = "application/json")
 class QuoteController {
 
     private final QuoteClient client;
@@ -42,11 +46,11 @@ class QuoteController {
         this.client = client;
     }
 
-    @GetMapping("quote")
+    @GetMapping(value = "quote")
     public ResponseEntity<Quote> getQuote() {
         return ResponseEntity.ok(client.fetchRandomQuote());
     }
-    @GetMapping("quotes")
+    @GetMapping(value = "quotes")
     public ResponseEntity<List<Quote>> getQuotes() {
         return ResponseEntity.ok(client.fetchRandomQuotes());
     }
