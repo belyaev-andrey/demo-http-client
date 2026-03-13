b1d80cbc5e75326da794c537264614814b928edc
Andrey Belyaev <belyaev.andrey@gmail.com>
2026-03-12 23:01:16 -0700

    Added AOT image build

--- http-server/src/main/java/com/jetbrains/test/boot4/server/HttpServerApplication.java
+++ http-server/src/main/java/com/jetbrains/test/boot4/server/HttpServerApplication.java
@@ -1,16 +1,26 @@
 package com.jetbrains.test.boot4.server;
 
 import com.jetbrains.test.boot4.server.quote.QuoteProviderRegistrar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Import(QuoteProviderRegistrar.class)
 public class HttpServerApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpServerApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(HttpServerApplication.class, args);
+        var app = SpringApplication.run(HttpServerApplication.class, args);
+        ConfigurableEnvironment environment = app.getEnvironment();
+        String appName = environment.getProperty("spring.application.name");
+        log.info("Application Name: {}", appName);
     }
 
 }
